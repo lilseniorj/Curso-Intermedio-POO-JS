dda74a6ef5c621ca33b872adeffce73d461fa2d9
Jesus Vargas <jesusvarguer18@gmail.com>
2022-04-17 20:44:29 -0500

    añadiendo la configuracion de VsCode con GitHub

---
+++ main.js
@@ -0,0 +1,23 @@
+const juan = {
+  name: "Juanito",
+  age: 18,
+  approvedCourses: ["Curso 1"],
+  addCourse(newCourse) {
+    console.log("This", this);
+    console.log("This.approvedCourses", this.approvedCourses);
+    this.approvedCourses.push(newCourse);
+  }
+};
+
+// console.log(Object.keys(juan));
+// console.log(Object.getOwnPropertyNames(juan));
+// console.log(Object.entries(juan));
+
+console.log(Object.getOwnPropertyDescriptors(juan));
+
+Object.defineProperty(juan, "pruebaNASA", {
+  value: "Extraterestres",
+  writable: true,
+  enumerable: true,
+  configurable: true,
+});
