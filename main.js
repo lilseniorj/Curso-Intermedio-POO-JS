874a962efbb62be1ec19df676059e4340f1b0629
Jesus Vargas <jesusvarguer18@gmail.com>
2022-04-23 18:36:05 -0500

    Subiendo los archivos completos y terminados

--- main.js
+++ main.js
@@ -1,23 +1,139 @@
-const juan = {
-  name: "Juanito",
-  age: 18,
-  approvedCourses: ["Curso 1"],
-  addCourse(newCourse) {
-    console.log("This", this);
-    console.log("This.approvedCourses", this.approvedCourses);
-    this.approvedCourses.push(newCourse);
+function isObject(subject) {
+  return typeof subject == "object";
+}
+
+function isArray(subject) {
+  return Array.isArray(subject);
+}
+
+function deepCopy(subject) {
+  let copySubject;
+
+  const subjectIsObject = isObject(subject);
+  const subjectIsArray = isArray(subject);
+
+  if (subjectIsArray) {
+    copySubject = [];
+  } else if (subjectIsObject) {
+    copySubject = {};
+  } else {
+    return subject;
   }
-};
 
-// console.log(Object.keys(juan));
-// console.log(Object.getOwnPropertyNames(juan));
-// console.log(Object.entries(juan));
+  for (key in subject) {
+    const keyIsObject = isObject(subject[key]);
 
-console.log(Object.getOwnPropertyDescriptors(juan));
+    if (keyIsObject) {
+      copySubject[key] = deepCopy(subject[key]);
+    } else {
+      if (subjectIsArray) {
+        copySubject.push(subject[key]);
+      } else {
+        copySubject[key] = subject[key];
+      }
+    }
+  }
 
-Object.defineProperty(juan, "pruebaNASA", {
-  value: "Extraterestres",
-  writable: true,
-  enumerable: true,
-  configurable: true,
+  return copySubject;
+}
+
+
+function SuperObject() {}
+SuperObject.isObject = function (subject) {
+  return typeof subject !== "object";
+}
+SuperObject.deepCopy = function (subject) {
+  let copySubject;
+
+  const subjectIsObject = isObject(subject);
+  const subjectIsArray = isArray(subject);
+
+  if (subjectIsArray) {
+    copySubject = [];
+  } else if (subjectIsObject) {
+    copySubject = {};
+  } else {
+    return subject;
+  }
+
+  for (key in subject) {
+    const keyIsObject = isObject(subject[key]);
+
+    if (keyIsObject) {
+      copySubject[key] = deepCopy(subject[key]);
+    } else {
+      if (subjectIsArray) {
+        copySubject.push(subject[key]);
+      } else {
+        copySubject[key] = subject[key];
+      }
+    }
+  }
+
+  return copySubject;
+}
+
+function requiredParam(param) {
+  throw new Error(param + " es obligatorio");
+}
+
+function LearningPath({
+  name = requiredParam("name"),
+  courses = [],
+}) {
+  this.name = name;
+  this.courses = courses;
+}
+
+function Student({
+  name = requiredParam("name"),
+  email = requiredParam("email"),
+  age,
+  twitter,
+  instagram,
+  facebook,
+  approvedCourses = [],
+  learningPaths = [],
+} = {}) {
+  this.name = name;
+  this.email = email;
+  this.age = age;
+  this.approvedCourses = approvedCourses;
+  this.socialMedia = {
+    twitter,
+    instagram,
+    facebook,
+  };
+
+  const private = {
+    "_learningPaths": [],
+  };
+
+  Object.defineProperty(this, "learningPaths", {
+    get() {
+      return private["_learningPaths"];
+    },
+    set(newLp) {
+      if (newLp instanceof LearningPath) {
+        private["_learningPaths"].push(newLp);
+      } else {
+        console.warn("Alguno de los LPs no es una instancia dell prototipo LearningPath");
+      }
+    },
+  });
+
+  for (learningPathIndex in learningPaths) {
+    this.learningPaths = learningPaths[learningPathIndex];
+  }
+}
+
+const escuelaWeb = new LearningPath({ name: "Escuela de WebDev" });
+const escuelaData = new LearningPath({ name: "Escuela de Data Science" });
+const juan = new Student({
+  email: "juanito@frijoles.co",
+  name: "Juanito",
+  learningPaths: [
+    escuelaWeb,
+    escuelaData,
+  ],
 });
